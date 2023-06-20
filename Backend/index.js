e0f2e3dbbf879d2cd51076f566d61bb4292af02e
AHZHONG <leolai0224@gmail.com>
2023-06-20 23:02:00 +0800

    init all

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,25 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
+
+app.listen(port, () => console.log(`Server listening at port ${port}`));
+
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose.connect(
+    process.env.MONGODB_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+mongoose.connection.on('connected', function(){console.log("connection, successful")});
 
 app.get("/api", (req, res) => {
     res.send("Hello World from Express");
 });
 
-app.listen(port, () => console.log(`Server listening at port ${port}`));
